647a9522bf802fc7b1b09ce245ef7bc977312452
Oleksandr <volgabasss@gmail.com>
2024-03-05 22:34:55 +0200

    added first version of user profile

---
+++ Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcApp.Models;
+using AppContext = MvcApp.Models.AppContext;
+
+namespace MvcApp.Controllers
+{
+
+    public class UserController : Controller
+    {
+        public IActionResult Profile(string username)
+        {
+            ViewData["username"] = username;
+            return View();
+        }
+        public IActionResult Settings()
+        {
+            return View();
+        }
+    }
+}
