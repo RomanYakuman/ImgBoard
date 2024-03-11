be18a981377de870543bc2fe5b69ba3bfed782a2
Oleksandr <volgabasss@gmail.com>
2024-03-11 22:24:38 +0200

    added links to all user`s posts(without pagination) and comments

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using AppContext = MvcApp.Models.AppContext;
+﻿using AppContext = MvcApp.Models.AppContext;
+using Microsoft.AspNetCore.Mvc;
+using MvcApp.Models;
 
 namespace MvcApp.Controllers;
 
@@ -14,12 +15,32 @@ public class UserController : Controller
         }
         using (AppContext db = new())
         {
-            ViewBag.User = db.Users.FirstOrDefault(u => u.username == username);
+            var user = db.Users.FirstOrDefault(u => u.username == username);
+            ViewBag.User = user;
+            ViewData["uploads"] = db.Posts.
+                Where(p => p.user == user.username).Count();
+            ViewData["comments"] = db.Comments.
+                Where(c => c.user == user.username).Count();
         }
         return View();
     }
-    public IActionResult Settings()
+    public IActionResult Comments(string username)
     {
+        using (AppContext db = new())
+        {
+            var comments = db.Comments.Where(c => c.user == username)
+                .OrderByDescending(c => c.time_created).ToList();
+            ViewBag.comments = comments;
+        }
+        return View();
+    }
+    public IActionResult Posts(string username, int id = 1)
+    {
+        using (AppContext db = new())
+        {
+            ViewBag.Posts = db.Posts.Where(p => p.user == username)
+                .OrderByDescending(x =>  x.id).ToList();
+        }
         return View();
     }
 }
